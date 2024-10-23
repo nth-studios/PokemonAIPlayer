9601c738f9d8b09976395544a0c74dfb93758e2a
Nicholas Brigando <nicholas@gendac.co.za>
2024-10-23 19:47:26 +0200

    Running with random inputs. Step 1!

--- Red/main.py
+++ Red/main.py
@@ -1,14 +1,41 @@
+import random
+import time
+from pytesseract import pytesseract
 from pyboy import PyBoy
+# import tensorflow as tf
 
 pyboy = PyBoy('red.gb')
+pyboy.set_emulation_speed(0)
 
 with open("state_file.state", "rb") as f:
     pyboy.load_state(f)
 
+time_train = 60*60*60
 
-while pyboy.tick():
-    print("playing")
-    pass
+outputs = ["a", "b", "start", "select", "left", "right", "up", "down"]
+
+red = pyboy.game_wrapper
+red.start_game()
+
+count = 0
+
+random.seed(time.process_time())
+
+while count < time_train:
+    pyboy.tick(24)
+    image = pyboy.screen.image
+
+    resize = image.resize((40, 36))
+    # image.save("Training Frames/" + "frame" + str(count) + ".png")
+    # resize.save("Training Frames/" + "frame" + str(count) + ".png")
+    input = random.randint(0, 7)
+    print(input)
+    pyboy.button(outputs[input], 12)
+    count = count + 1
+
+
+# while pyboy.tick():
+#     pass
 
 # print("Saving")
 # with open("state_file.state", "wb") as f:
